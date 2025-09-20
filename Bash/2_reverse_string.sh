fb0032531944e42bc16ef673ae64b55ad798cf03
Zarei <mrz.jaliani@gmail.com>
2025-09-20 18:47:54 +0330

    exercise2_reverseString

--- Bash/2_reverse_string.sh
+++ Bash/2_reverse_string.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 read -p "Enter your string: " myString
 reverseString=""
-for i in $myString; do
-    reverseString="$i $reverseString"
+length=${#myString}
+for ((i=$length-1; i>=0; i--)); do
+    reverseString="${reverseString}${myString:$i:1}"
 done
 echo $reverseString
