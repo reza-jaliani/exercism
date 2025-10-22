cab179caf64c613fdc1626e85611548edc6545ba
Reza Jaliani <mrz.jaliani@gmail.com>
2025-10-22 06:12:54 +0330

    Create 15_raindrops.sh

---
+++ Bash/15_raindrops.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+main () {
+    input=${1:-}
+    sound=false
+    if (( $input % 3 == 0 )); then
+        echo -n "Pling"
+        sound=true
+    fi
+    if (( $input % 5 == 0 )); then
+        echo -n "Plang"
+        sound=true
+    fi
+    if (( $input % 7 == 0 )); then
+        echo -n "Plong"
+        sound=true
+        exit 0
+    fi
+    if [[ $sound == false ]]; then
+        echo "$input"
+    fi
+}
+
+main "$@"
