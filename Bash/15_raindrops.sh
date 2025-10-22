27c44acd32cb432c5d29078c20a579a01338da24
Reza Jaliani <mrz.jaliani@gmail.com>
2025-10-22 06:14:22 +0330

    Update 15_raindrops.sh
    remove unnecessary $ chars in if

--- Bash/15_raindrops.sh
+++ Bash/15_raindrops.sh
@@ -3,15 +3,15 @@
 main () {
     input=${1:-}
     sound=false
-    if (( $input % 3 == 0 )); then
+    if (( input % 3 == 0 )); then
         echo -n "Pling"
         sound=true
     fi
-    if (( $input % 5 == 0 )); then
+    if (( input % 5 == 0 )); then
         echo -n "Plang"
         sound=true
     fi
-    if (( $input % 7 == 0 )); then
+    if (( input % 7 == 0 )); then
         echo -n "Plong"
         sound=true
         exit 0
